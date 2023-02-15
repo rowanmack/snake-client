b4fc66993969c54dbdbd9391fffdef8659905f08
Rowan Mackenzie <row.mackenzie@gmail.com>
2023-02-15 12:49:08 -0800

    added write nameand connected to server message

--- client.js
+++ client.js
@@ -1,14 +1,24 @@
 const net = require("net");
 
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: '50541'
   });
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to the game server")
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: RoW");
+  });
+
   conn.on('data', (data) => {
     console.log(data);
-  })
+  });
+
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
